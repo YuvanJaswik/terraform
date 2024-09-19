3922230e2ced4c2c2948419d542a654b6b42b2fa
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-09-19 17:54:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,11 +5,4 @@ terraform {
      version = "~> 2.4.11"
    }
  }
-   backend "s3" {
-     bucket         = "myterraformstate-bucket"
-     key            = "root/AWS-Terraform/tf-tutsy/terraform.tfstate"
-     region         = "us-east-1"
-     encrypt        = true
-
-}
 }
