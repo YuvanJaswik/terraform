30e9b1fd990ae01d6b1eb79e563683f10169e42d
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-09-19 18:18:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-resource "aws_instance" "my_vm" {
- ami           = var.ami //Ubuntu AMI
- instance_type = var.instance_type
+resource "aws_instance" "example" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
- tags = {
-   Name = var.name_tag,
- }
+  tags = {
+    Name = var.name_tag
+  }
 }
-
