361c177734990108e225e7c8dd61c28c9c3ccea0
YuvanJaswik <arepallyrajavardhanreddy@gmail.com>
2024-09-19 18:10:07 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~> 4.18.0"
-    }
-  }
 provider "aws" {
-  region = "us-east-1"
-}
+  region = "us-west-2"
 }
